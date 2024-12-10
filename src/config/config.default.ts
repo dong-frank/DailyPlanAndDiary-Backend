bd73b611b37d45a965acad89b114ec8b0b2dfc84
ErinwithBMQ <631165975@qq.com>
2024-12-10 15:22:40 +0800

    12.6 token信息获取

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -26,7 +26,7 @@ export default {
         username: 'postgres',
         password: 'qxy246886420', // 改成你的数据库密码
         database: 'daily', // 改成你的数据库名
-        synchronize: true, // 新增加表时将其设置为true，其他时候（比如前端发送请求增加数据时）设置为false
+        synchronize: false, // 新增加表时将其设置为true，其他时候（比如前端发送请求增加数据时）设置为false
         logging: true,
         entities: [File_upload, User, Diary], // 实体类，每增加一个表，就增加一个，别忘了加
       },
